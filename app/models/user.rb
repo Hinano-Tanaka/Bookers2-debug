86b904baa51f59d154456e2204018febeccb1483
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-01-23 06:51:05 +0000

    [Update] modified view files

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :followed
 
 
   has_one_attached :profile_image
@@ -25,7 +26,7 @@ class User < ApplicationRecord
   end
 # -----------------------------------
   def follow(user_id)
-  relationships.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
@@ -33,7 +34,7 @@ class User < ApplicationRecord
   end
 
   def following?(user)
-  followings.include?(user)
+    followings.include?(user)
   end
-# ---------------------------------------
+# -----------------------------------
 end
